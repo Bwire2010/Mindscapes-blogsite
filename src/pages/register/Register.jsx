d3c7b5376a95f6b1e33dbf5a4f857c4fa1a778a4
Patrobas Bwire <patrobasbwire@gmail.com>
2023-02-15 12:16:44 +0300

    Done working on register and login page

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -2,7 +2,7 @@ import "./register.css";
 
 export default function Register() {
   return (
-    <div className="login">
+    <div className="register">
       <span className="registerTitle">Register</span>
       <form className="registerForm">
         <label>Username</label>
