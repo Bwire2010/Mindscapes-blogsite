2c580a4b8913ddd938daa4717ae7b23110c16cae
Patrobas Bwire <patrobasbwire@gmail.com>
2023-02-15 13:16:25 +0300

    done with routing

--- src/App.js
+++ src/App.js
@@ -5,13 +5,22 @@ import Write from "./pages/write/Write";
 import Settings from "./pages/settings/Settings";
 import Login from "./pages/Login/Login";
 import Register from "./pages/register/Register";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
+  const user = true;
   return (
-    <>
-      <TopBar/>
-      <Register/>
-    </>
+    <Router>
+      <TopBar />
+      <Routes>
+        <Route exact path="/" element={<Home />}></Route>
+        <Route path="/register" element={ user ? <Home /> : <Register />}></Route>
+        <Route path="/login" element={ user ? <Home /> : <Login />}></Route>
+        <Route path="/write" element={ user ? <Write /> : <Register />}></Route>
+        <Route path="/settings" element={ user ? <Settings /> : <Register />}></Route>
+        <Route path="/post/:postId" element={<Single />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
