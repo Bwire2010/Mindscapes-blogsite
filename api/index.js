c9ff680d3e345c9a4d4a908afaacafa1e893d752
Patrobas Bwire <patrobasbwire@gmail.com>
2023-02-17 08:30:11 +0300

    connected succesfult to the MongoDB

--- api/index.js
+++ api/index.js
@@ -1,5 +1,19 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose")
+
+dotenv.config();
+
+mongoose.set('strictQuery', false);
+
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(console.log("connected to MongoDB"))
+.catch(err=>console.log(err));
+
 
 
 app.listen("5000", () => {
