b6293d747c84aa2e0a1291177819a48cb62d0c93
Patrobas Bwire <patrobasbwire@gmail.com>
2023-02-21 11:45:01 +0300

    worked on creating and updating posts

--- api/index.js
+++ api/index.js
@@ -4,6 +4,8 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+
 
 
 dotenv.config();
@@ -21,6 +23,8 @@ mongoose
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+
 
 app.listen("5001", () => {
   console.log("Backend is running.");
