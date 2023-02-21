96a80003a7dc185f57b1dfcc096b5a089a6e69e4
Patrobas Bwire <patrobasbwire@gmail.com>
2023-02-21 16:47:31 +0300

    finished installing multer to enable file uploads

--- api/index.js
+++ api/index.js
@@ -6,8 +6,7 @@ const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
 const categoryRoute = require("./routes/categories");
-
-
+const multer = require("multer");
 
 dotenv.config();
 app.use(express.json());
@@ -21,6 +20,20 @@ mongoose
   })
   .then(console.log("connected to MongoDB"))
   .catch((err) => console.log(err));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, res.body.name);
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded");
+});
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
