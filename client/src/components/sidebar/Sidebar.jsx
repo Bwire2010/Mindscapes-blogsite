1c4314e89a91cb2a2b146f10189c8bd168479bde
Patrobas Bwire <patrobasbwire@gmail.com>
2023-03-03 10:28:42 +0300

    added categories

--- client/src/components/sidebar/Sidebar.jsx
+++ client/src/components/sidebar/Sidebar.jsx
@@ -1,6 +1,18 @@
+import { useEffect, useState } from "react";
 import "./sidebar.css";
+import axios from "axios";
 
 export default function Sidebar() {
+  const [cats, setCats] = useState([]);
+
+  useEffect(() => {
+    const getCats = async () => {
+      const res = await axios.get("/categories");
+      setCats(res.data);
+    };
+    getCats();
+  }, []);
+
   return (
     <div className="sidebar">
       <div className="sidebarItem">
@@ -19,24 +31,21 @@ export default function Sidebar() {
       <div className="sidebarItem">
         <span className="sidebarTitle">CATEGORIES</span>
         <ul className="sidebarList">
-            <li className="sidebarListItem">Tech</li>
-            <li className="sidebarListItem"> Cinema</li>
-            <li className="sidebarListItem">Music</li>
-            <li className="sidebarListItem">Sports</li>
-            <li className="sidebarListItem">Fashion</li>
-            <li className="sidebarListItem">Life</li>
+          {cats.map((c) => (
+            <li className="sidebarListItem">{c.name}</li>
+          ))}
         </ul>
       </div>
       <div className="sidebarItem">
-      <span className="sidebarTitle">FOLLOW US</span>
-      <div className="sidebarSocial">
+        <span className="sidebarTitle">FOLLOW US</span>
         <div className="sidebarSocial">
-        <i className="sidebarIcon fa-brands fa-square-facebook"></i>
-        <i className="sidebarIcon fa-brands fa-square-twitter"></i>
-        <i className="sidebarIcon fa-brands fa-linkedin"></i>
-        <i className="sidebarIcon fa-brands fa-square-instagram"></i>
+          <div className="sidebarSocial">
+            <i className="sidebarIcon fa-brands fa-square-facebook"></i>
+            <i className="sidebarIcon fa-brands fa-square-twitter"></i>
+            <i className="sidebarIcon fa-brands fa-linkedin"></i>
+            <i className="sidebarIcon fa-brands fa-square-instagram"></i>
+          </div>
         </div>
-      </div>
       </div>
     </div>
   );
